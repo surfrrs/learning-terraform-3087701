18b4e0228923f5b93fa6ffb6f301e6aa3af07a28
surfrrs <152572011+surfrrs@users.noreply.github.com>
2024-02-26 15:18:22 -0800

    alb

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog_new" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [module.blog_sgroup.security_group_id]
