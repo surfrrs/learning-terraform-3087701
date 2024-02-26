d48c786b70162ab0fe17ea533626c0fb33de686e
surfrrs <152572011+surfrrs@users.noreply.github.com>
2024-02-25 16:42:29 -0800

    fix ingress rules

--- main.tf
+++ main.tf
@@ -33,9 +33,10 @@ module "blog_sgroup" {
   source      = "terraform-aws-modules/security-group/aws"
   version     = "5.1.0"
   name        = "blog_new"
+  description = "Allow http and https in.  Allow everything out."
 
   vpc_id              = data.aws_vpc.default.id
-  ingress_rules       = ["http-80-tcp","http-443-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
 }
