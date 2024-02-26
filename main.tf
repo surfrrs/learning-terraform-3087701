53fd80825cd61e13add61b04a761143df54c61e8
surfrrs <152572011+surfrrs@users.noreply.github.com>
2024-02-25 18:50:05 -0800

    module subnets

--- main.tf
+++ main.tf
@@ -36,8 +36,9 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_sgroup.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
