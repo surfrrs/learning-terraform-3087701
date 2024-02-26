8f7887ffe1d88b90338062e6610651ec9ce7a2cc
surfrrs <152572011+surfrrs@users.noreply.github.com>
2024-02-25 18:41:06 -0800

    create new vpc

--- main.tf
+++ main.tf
@@ -18,6 +18,21 @@ data "aws_vpc" "default" {
   default = true 
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "blog_dev_environment"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -34,7 +49,7 @@ module "blog_sgroup" {
   version     = "5.1.0"
   name        = "blog_new"
 
-  vpc_id              = data.aws_vpc.default.id
+  vpc_id              = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
