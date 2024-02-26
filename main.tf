f0f0bd3ec19da85ed95e8db677590e4af19422b3
surfrrs <152572011+surfrrs@users.noreply.github.com>
2024-02-26 12:21:27 -0800

    create new vpc in dev

--- main.tf
+++ main.tf
@@ -14,10 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true 
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -25,6 +21,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -45,10 +42,9 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog_sgroup" {
+module "blog_sg" {
   source      = "terraform-aws-modules/security-group/aws"
   version     = "5.1.0"
-  name        = "blog_new"
 
   vpc_id              = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
